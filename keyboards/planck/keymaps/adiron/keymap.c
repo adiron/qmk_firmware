d26e4cce929395aed51a539c7c668892db1c3966
Adi Ron <adi@adiron.me>
2016-12-15 22:04:54 +0200

    Some keymap changes
    Added raise lock and lower lock with the spacebar.
    Changed the location of minus and equal from the top row to the middle
    and also added Cmd -/+

--- keyboards/planck/keymaps/adiron/keymap.c
+++ keyboards/planck/keymaps/adiron/keymap.c
@@ -33,21 +33,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_0, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS}
 },
 [_RAISE] = { /* RAISE */
- {KC_TRNS,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_DELETE},
- {FUNC(6), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPRV, KC_MPLY, KC_MNXT, KC_TRNS, KC_TRNS  },
- {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC, KC_BSLS, KC_TRNS  },
- {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS  }
+ {KC_TRNS,    KC_1,    KC_2,    KC_3,    KC_4,     KC_5,     KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_DELETE},
+ {FUNC(6), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_MPRV, KC_MPLY, KC_MNXT, KC_TRNS, KC_TRNS  },
+ {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC, KC_BSLS, KC_TRNS  },
+ {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, FUNC(10), FUNC(10), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS  }
 },
 [_LOWER] = { /* LOWER */
  //                                            HOMING F                         HOMING J
  {  KC_F1,   KC_F2,   KC_F3,        KC_F4,        KC_F5,        KC_F6,   KC_F7,    KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12},
  {KC_TRNS, KC_VOLU,   BL_INC, KC_MS_ACCEL0, KC_MS_ACCEL1, KC_MS_ACCEL2,    M(0),    M(1), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS},
  {KC_TRNS, KC_VOLD,   BL_DEC,      KC_TRNS,      KC_TRNS,      KC_TRNS, KC_TRNS,  MU_TOG,  AU_TOG, KC_TRNS, KC_TRNS, KC_TRNS},
- {  RESET, KC_TRNS,  KC_TRNS,      KC_TRNS,      KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS}
+ {  RESET, KC_TRNS,  KC_TRNS,      KC_TRNS,      KC_TRNS,      FUNC(9), FUNC(9), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS}
 },
 [_ENTFN] = { /* Enter-FN */
- {KC_CAPS, KC_TRNS, KC_TRNS,     KC_TRNS,       KC_TRNS,     KC_TRNS,     KC_TRNS, KC_BTN1, KC_BTN2,  KC_TRNS,  KC_TRNS, KC_TRNS},
- {KC_ESC , KC_TRNS, KC_WH_L, KC_MS_WH_UP, KC_MS_WH_DOWN,     KC_WH_R,     KC_LEFT, KC_DOWN,   KC_UP, KC_RIGHT, KC_MINUS, KC_EQUAL},
+ {KC_CAPS, KC_TRNS, KC_TRNS,     KC_TRNS,       KC_TRNS,     KC_TRNS,     KC_TRNS, KC_BTN1, KC_BTN2,  KC_TRNS, KC_MINUS, KC_EQUAL},
+ {KC_ESC , KC_TRNS, KC_WH_L, KC_MS_WH_UP, KC_MS_WH_DOWN,     KC_WH_R,     KC_LEFT, KC_DOWN,   KC_UP, KC_RIGHT,     M(2), M(3)},
  {KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,       KC_TRNS,     KC_TRNS,     KC_PGDN, KC_PGUP, KC_HOME,   KC_END,  KC_TRNS, KC_TRNS},
  {FUNC(4), KC_TRNS, KC_TRNS,     KC_TRNS,       KC_TRNS, FUNC(SPCLK), FUNC(SPCLK), KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS}
 },
@@ -93,6 +93,17 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
         return MACRO( I(1), T(J), T(J), T(J), END  );
       }
       break;
+    case 2:
+      if (record->event.pressed) {
+        return MACRO( I(1), D(RGUI), T(MINUS), U(RGUI), END  );
+      }
+      break;
+    case 3:
+      if (record->event.pressed) {
+        return MACRO( I(1), D(RGUI), T(EQUAL), U(RGUI), END  );
+      }
+      break;
+
   }
   return MACRO_NONE;
 };
