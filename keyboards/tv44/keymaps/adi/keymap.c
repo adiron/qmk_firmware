f71d2e3b3b17b16fbde8946d6838f4411fbfb35e
Adi Ron <adi@adiron.me>
2018-01-26 10:27:12 +0200

    Removed space cadet, it was causeing problems

--- keyboards/tv44/keymaps/adi/keymap.c
+++ keyboards/tv44/keymaps/adi/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [QWERTY] = {
         {KC_FN5,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_BSPC},
         {KC_TAB,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,  KC_FN4,KC_FN2},
-        {KC_LSPO,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,  KC_COMM,KC_DOT,KC_SLSH,KC_RSPC},
+        {KC_LSFT,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,  KC_COMM,KC_DOT,KC_SLSH,KC_RSFT},
         {KC_LCTL,  KC_LALT,  KC_LGUI, KC_SPC, XXXXXXX, XXXXXXX, XXXXXXX,    KC_FN0, KC_FN1, KC_RGUI, XXXXXXX, KC_RCTL}
     },
     [RAISE] = {
