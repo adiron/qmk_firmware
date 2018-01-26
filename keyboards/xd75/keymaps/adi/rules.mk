d5dd9027eb740d4ef67b7a1c0389debef16b6cf6
Adi Ron <adi@adiron.me>
2018-01-26 10:14:17 +0200

    Changed rules.mk for xd75

--- keyboards/xd75/keymaps/adi/rules.mk
+++ keyboards/xd75/keymaps/adi/rules.mk
@@ -2,6 +2,11 @@ NKRO_ENABLE = yes           # Nkey Rollover - if this doesn't work, see here: ht
 TAP_DANCE_ENABLE = no
 MIDI_ENABLE = yes
 AUDIO_ENABLE = no
+CONSOLE_ENABLE = no
+BLUETOOTH_ENABLE = no
+BACKLIGHT_ENABLE = no
+BOOTMAGIC_ENABLE = no
+RGBLIGHT_ENABLE = yes
 
 ifndef QUANTUM_DIR
 	include ../../../../Makefile
