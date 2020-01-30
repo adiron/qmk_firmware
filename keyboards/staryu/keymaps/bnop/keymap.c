f9dd94a47a2ef1857a83987bf32214d96dc431a9
Adi Ron <adi@bnop.co>
2020-01-30 09:52:50 +0200

    Excuses added lol

--- keyboards/staryu/keymaps/bnop/keymap.c
+++ keyboards/staryu/keymaps/bnop/keymap.c
@@ -7,14 +7,22 @@ bool has_layer_changed = false;
 static uint8_t current_layer;
 
 enum custom_keycodes {
-  DANI = SAFE_RANGE,
-  BACON,
-  HOVAV,
-  OPPENHEIM,
-  EXCUSE1,
+  EXCUSE1 = SAFE_RANGE,
   EXCUSE2,
   EXCUSE3,
   EXCUSE4,
+  EXCUSE5,
+  EXCUSE6,
+  EXCUSE7,
+  EXCUSE8,
+  EXCUSE9,
+  EXCUSE10,
+  EXCUSE11,
+  EXCUSE12,
+  //   EXCUSE13,
+  //   EXCUSE14,
+  //   EXCUSE15,
+  //   EXCUSE16,
 };
 
 enum layers {
@@ -26,16 +34,16 @@ enum layers {
 
 const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
   LAYOUT(
-    /* EMPTY  */ KC_B,  KC_N,
-    TO(_LAYER1), KC_O, KC_P
-  ),
-  LAYOUT(
-    /* EMPTY  */ DANI,  BACON,
-    TO(_LAYER2), HOVAV, OPPENHEIM
-  ),
-  LAYOUT(
     /* EMPTY  */ EXCUSE1, EXCUSE2,
-    TO(_LAYER0), EXCUSE3, EXCUSE4
+    TO(_LAYER1), EXCUSE3, EXCUSE4
+  ),
+  LAYOUT(
+    /* EMPTY  */ EXCUSE5, EXCUSE6,
+    TO(_LAYER2), EXCUSE7, EXCUSE8
+  ),
+  LAYOUT(
+    /* EMPTY  */ EXCUSE9, EXCUSE10,
+    TO(_LAYER0), EXCUSE11, EXCUSE12
   )
 };
 
@@ -45,41 +53,9 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case DANI:
-      if (record->event.pressed) {
-        SEND_STRING("Dani ");
-      } else {
-        // when keycode is released
-      }
-      break;
-
-    case BACON:
-      if (record->event.pressed) {
-        SEND_STRING("Bacon ");
-      } else {
-        // when keycode is released
-      }
-      break;
-
-    case HOVAV:
-      if (record->event.pressed) {
-        SEND_STRING("Hovav ");
-      } else {
-        // when keycode is released
-      }
-      break;
-
-    case OPPENHEIM:
-      if (record->event.pressed) {
-        SEND_STRING("Oppenheim ");
-      } else {
-        // when keycode is released
-      }
-      break;
-
     case EXCUSE1:
       if (record->event.pressed) {
-        SEND_STRING("Unfortunately I can't make it, so you'll have to do it without me");
+        SEND_STRING("thzv pruhey bjns ungbhhi' tck pauy frdg tbjbu kt kuejho pruheyho c,juo vzv");
       } else {
         // when keycode is released
       }
@@ -87,7 +63,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
     case EXCUSE2:
       if (record->event.pressed) {
-        SEND_STRING("Thanks for the invitation, but I forgot to update my calendar and it clashes with something else I have.");
+        SEND_STRING("anjbu kanug gk vpruhey akfo! neuuho a,nmtu xyushu thbyrteyhc ahhej tu,u");
       } else {
         // when keycode is released
       }
@@ -95,7 +71,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
     case EXCUSE3:
       if (record->event.pressed) {
-        SEND_STRING("Oh no, is it this week? This week's no good, I think I'll sit this one out");
+        SEND_STRING("tbjbu kt kuejho pruhey cachk pju, nnkhui sukr xurh");
       } else {
         // when keycode is released
       }
@@ -103,7 +79,71 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
     case EXCUSE4:
       if (record->event.pressed) {
-        SEND_STRING("I will definitely come to this if I have time from the other meeting that I have that's at exactly the same time");
+        SEND_STRING("tbjbu frdg guaho re prhby tz brtv kh pju, n,tho");
+      } else {
+        // when keycode is released
+      }
+      break;
+
+    case EXCUSE5:
+      if (record->event.pressed) {
+        SEND_STRING("scru th,bu cxcc vdhux vct akfo");
+      } else {
+        // when keycode is released
+      }
+      break;
+
+    case EXCUSE6:
+      if (record->event.pressed) {
+        SEND_STRING("zv nuako ffv' tbjbu hfukho re kvrux kfo! tk ,abu scr! chh");
+      } else {
+        // when keycode is released
+      }
+      break;
+
+    case EXCUSE7:
+      if (record->event.pressed) {
+        SEND_STRING("tbjbu cshue gucsho go jcrv tjr, aguav t, tu,u vscr at,o guaho re hu,r yuc uzv hvhv bhdus gbhhbho///");
+      } else {
+        // when keycode is released
+      }
+      break;
+
+    case EXCUSE8:
+      if (record->event.pressed) {
+        SEND_STRING("tbjbu cab, ac,ui go zdnhhxyr b,rtv tjrh zv");
+      } else {
+        // when keycode is released
+      }
+      break;
+
+    case EXCUSE9:
+      if (record->event.pressed) {
+        SEND_STRING("kmgrbu kt bufk kej, pruheyho fk fl xjhho gnfo vxkhjv");
+      } else {
+        // when keycode is released
+      }
+      break;
+
+    case EXCUSE10:
+      if (record->event.pressed) {
+        SEND_STRING("tbjbu kt gucsho go kpkpho fnufo");
+      } else {
+        // when keycode is released
+      }
+      break;
+
+    case EXCUSE11:
+      if (record->event.pressed) {
+        SEND_STRING("nv zv?? kt ang,h?? tbh bfbx knbvrv uthi ekhyv!!!!");
+      } else {
+        // when keycode is released
+      }
+      break;
+
+    case EXCUSE12:
+      if (record->event.pressed) {
+        SEND_STRING("vvmgv vzt, ngkhcv t, vhfuku, akbu ftnbh hu-thex utbjbu nxrcho cfk ,ue;!");
       } else {
         // when keycode is released
       }
