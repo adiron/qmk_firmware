8c612c70d99bfc81dfd1d17002495898ca832ee2
Adi Ron <adi@adiron.me>
2022-09-21 19:15:10 +0300

    Updated the contra keymap to new standards

--- keyboards/contra/keymaps/adi/keymap.c
+++ keyboards/contra/keymaps/adi/keymap.c
@@ -8,46 +8,46 @@ extern keymap_config_t keymap_config;
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
-#define _QWERTY 0
-#define _LOWER 3
-#define _RAISE 4
-#define _PLOVER 5
-#define _ENTFN 6
-#define _MEDIA 7
-#define _MOUSE 8
-#define _ADJUST 16
-#define _NUMPAD 9
+enum planck_layers {
+    _QWERTY,
+    _LOWER,
+    _RAISE,
+    _ADJUST,
+    _ENTFN,
+    _MEDIA,
+    _MOUSE,
+    _NUMPAD
+};
 
-#define M_BACK 0
-#define M_FWRD 1
-#define M_PTAB 2
-#define M_NTAB 3
-#define M_ZOUT 4
-#define M_ZOIN 5
-#define M_ENTR 6
-#define M_SCRN 7
-#define M_NSPC 8
-#define M_PSPC 9
+#define F_ENTFN LT(_ENTFN, KC_ENT)
+#define F_MOUSE LT(_MOUSE, KC_SCLN)
+#define F_QUOTE LT(_MEDIA, KC_QUOT)
+#define F_NUMPAD MO(_NUMPAD)
 
 enum planck_keycodes {
   QWERTY = SAFE_RANGE,
-  PLOVER,
   LOWER,
   RAISE,
-  EXT_PLV
+  M_BACK,
+  M_FWRD,
+  M_PTAB,
+  M_NTAB,
+  M_ZOUT,
+  M_ZOIN,
+  M_ENTR,
+  M_NSPC,
+  M_PSPC,
+  M_APPN,
 };
 
-// Fillers to make layering more clear
-#define _______ KC_TRNS
-#define XXXXXXX KC_NO
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = {
   {KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
-  {KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    FUNC(3), FUNC(4)},
+  {KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    F_MOUSE, F_QUOTE},
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT},
-  {KC_LCTL, FUNC(2), KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   FUNC(1), KC_RGUI, KC_RALT, KC_RCTRL}
+  {KC_LCTL, F_NUMPAD, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   F_ENTFN, KC_RGUI, KC_RALT, KC_RCTRL}
 },
 
 [_LOWER] = {
@@ -64,26 +64,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {_______, _______, _______, _______, _______, _______, _______, _______, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS}
 },
 
-
-[_PLOVER] = {
-  {XXXXXXX, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC},
-  {XXXXXXX, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
-  {XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX},
-  {EXT_PLV, XXXXXXX, XXXXXXX, KC_C,    KC_V,    XXXXXXX, XXXXXXX, KC_N,    KC_M,    XXXXXXX, XXXXXXX, XXXXXXX}
-},
-
 [_ENTFN] = {
-  {_______, M(M_PTAB), M(M_NTAB), M(M_PSPC), M(M_NSPC),  _______, _______, KC_BTN1, KC_BTN2, M(M_ZOUT), M(M_ZOIN), _______},
-  { KC_ESC, _______, KC_MS_WH_LEFT, KC_MS_WH_UP, KC_MS_WH_DOWN, KC_MS_WH_RIGHT, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, _______, M(M_ENTR)},
-  {_______, M(M_BACK),  M(M_FWRD), _______, _______, _______, _______, _______, _______, _______,  _______, _______},
+  {_______, M_PTAB, M_NTAB, M_PSPC, M_NSPC,  _______, _______, KC_BTN1, KC_BTN2, M_ZOUT, M_ZOIN, _______},
+  { KC_ESC, _______, KC_MS_WH_LEFT, KC_MS_WH_UP, KC_MS_WH_DOWN, KC_MS_WH_RIGHT, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, _______, M_ENTR},
+  {_______, M_BACK,  M_FWRD, _______, _______, _______, _______, _______, _______, _______,  _______, _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______}
 },
 
 [_ADJUST] = {
   {_______, RESET  , _______, _______, _______, _______  , _______  , _______, _______, _______, _______, KC_DEL },
-  {_______, _______, _______, _______, _______, _______  , _______  , _______, _______, _______,  PLOVER, _______},
+  {_______, _______, _______, _______, _______, _______  , _______  , _______, _______, _______,  _______, _______},
   {_______, _______, _______, _______, _______, _______  , _______  , _______, _______, _______, _______, _______},
-  {_______, _______, _______, _______, _______, M(M_SCRN), M(M_SCRN), _______, _______, _______, AG_NORM, AG_SWAP}
+  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, AG_NORM, AG_SWAP}
 },
 
 [_MOUSE] = { /* Mousekeys */
@@ -109,102 +101,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
-    keyevent_t event = record->event;
-
-    if (!eeconfig_is_enabled()) {
-      eeconfig_init();
-    }
-    bool use_cmd = true;    // Use, for example, Cmd-Tab, Cmd-C, Cmd-V, etc.
-    // Compare to MAGIC_SWAP_ALT_GUI and MAGIC_UNSWAP_ALT_GUI configs, set in:
-    // quantum/quantum.c
-    if(keymap_config.swap_lalt_lgui == 1 && keymap_config.swap_ralt_rgui == 1) {
-      use_cmd = false;      // ... or, Alt-Tab, Ctrl-C, Ctrl-V, etc.
-    }
-    switch (id) {
-        case M_BACK:
-            /* Command + [ or previous page */
-            if (use_cmd) {
-                return (event.pressed ? MACRO( D(LGUI), T(LBRC), U(LGUI), END ) : MACRO(END));
-            } else {
-                return (event.pressed ? MACRO( D(LALT), T(LEFT), U(LALT), END ) : MACRO(END));
-            }
-            break;
-        case M_FWRD:
-            /* Command + ] or next page */
-            if (use_cmd) {
-                return (event.pressed ? MACRO( D(LGUI), T(RBRC), U(LGUI), END ) : MACRO(END));
-            } else {
-                return (event.pressed ? MACRO( D(LALT), T(RIGHT), U(LALT), END ) : MACRO(END));
-            }
-            break;
-        case M_PTAB:
-            /* Command + { or prev tab. */
-            if (use_cmd) {
-                return (event.pressed ? MACRO( D(LGUI), D(RSFT), T(LBRC), U(RSFT), U(LGUI), END ) : MACRO(END));
-            } else {
-                return (event.pressed ? MACRO( D(LCTRL), D(RSFT), T(TAB), U(RSFT), U(LCTRL), END ) : MACRO(END));
-            }
-            break;
-        case M_NTAB:
-            /* Command + } or next tab*/
-            if (use_cmd) {
-                return (event.pressed ? MACRO( D(LGUI), D(RSFT), T(RBRC), U(RSFT), U(LGUI), END ) : MACRO(END));
-            } else {
-                return (event.pressed ? MACRO( D(LCTRL), T(TAB), U(LCTRL), END ) : MACRO(END));
-            }
-            break;
-        case M_ZOUT:
-            /* Command + - or Ctrl + -*/
-            if (use_cmd) {
-                return (event.pressed ? MACRO( D(LGUI), T(MINS), U(LGUI), END ) : MACRO(END));
-            } else {
-                return (event.pressed ? MACRO( D(LCTRL), T(MINS), U(LCTRL), END ) : MACRO(END));
-            }
-            break;
-        case M_ZOIN:
-            /* Command + = or Ctrl + =*/
-            if (use_cmd) {
-                return (event.pressed ? MACRO( D(LGUI), T(EQL), U(LGUI), END ) : MACRO(END));
-            } else {
-                return (event.pressed ? MACRO( D(LCTRL), T(EQL), U(LCTRL), END ) : MACRO(END));
-            }
-            break;
-        case M_ENTR:
-            /* Command + Enter or Ctrl + Enter*/
-            if (use_cmd) {
-                return (event.pressed ? MACRO( D(LGUI), T(ENT), U(LGUI), END ) : MACRO(END));
-            } else {
-                return (event.pressed ? MACRO( D(LCTRL), T(ENT), U(LCTRL), END ) : MACRO(END));
-            }
-        case M_SCRN:
-            /* Command + Enter or Ctrl + Enter*/
-            if (use_cmd) {
-                return (event.pressed ? MACRO( D(LGUI), D(LCTRL), D(LSFT), T(4), U(LSFT), U(LGUI), U(LCTRL), END ) : MACRO(END));
-            } else {
-                return (event.pressed ? MACRO( T(PSCR) ) : MACRO(END));
-            }
-        case M_NSPC:
-            /* Next space / desktop */
-            if (use_cmd) {
-                return (event.pressed ? MACRO( D(LCTRL), T(RIGHT), U(LCTRL), END ) : MACRO(END));
-            } else {
-                return (event.pressed ? MACRO( D(LGUI), T(RIGHT), U(LGUI), END ) : MACRO(END));
-            }
-        case M_PSPC:
-            /* Previous space / desktop */
-            if (use_cmd) {
-                return (event.pressed ? MACRO( D(LCTRL), T(LEFT), U(LCTRL), END ) : MACRO(END));
-            } else {
-                return (event.pressed ? MACRO( D(LGUI), T(LEFT), U(LGUI), END ) : MACRO(END));
-            }
-        default:
-            break;
-    }
-    return MACRO_NONE;
-}
-
-
 void persistant_default_layer_set(uint16_t default_layer) {
   eeconfig_update_default_layer(default_layer);
   default_layer_set(default_layer);
@@ -218,6 +114,16 @@ const uint16_t PROGMEM fn_actions[] = {
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (!eeconfig_is_enabled()) {
+        eeconfig_init();
+    }
+    bool use_cmd = true; // Use, for example, Cmd-Tab, Cmd-C, Cmd-V, etc.
+    // Compare to MAGIC_SWAP_ALT_GUI and MAGIC_UNSWAP_ALT_GUI configs, set in:
+    // quantum/quantum.c
+    if (keymap_config.swap_lalt_lgui == 1 && keymap_config.swap_ralt_rgui == 1) {
+        use_cmd = false; // ... or, Alt-Tab, Ctrl-C, Ctrl-V, etc.
+    }
+
   switch (keycode) {
     case LOWER:
       if (record->event.pressed) {
@@ -239,27 +145,106 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
-    case PLOVER:
-      if (record->event.pressed) {
-        layer_off(_RAISE);
-        layer_off(_LOWER);
-        layer_off(_ADJUST);
-        layer_on(_PLOVER);
-        if (!eeconfig_is_enabled()) {
-            eeconfig_init();
+    case M_BACK:
+        /* Command + [ or previous page */
+        if (record->event.pressed) {
+            if (use_cmd) {
+                SEND_STRING(SS_LGUI(SS_TAP(X_LBRC)));
+            } else {
+                SEND_STRING(SS_LALT(SS_TAP(X_LEFT)));
+            }
         }
-        keymap_config.raw = eeconfig_read_keymap();
-        keymap_config.nkro = 1;
-        eeconfig_update_keymap(keymap_config.raw);
-      }
-      return false;
-      break;
-    case EXT_PLV:
-      if (record->event.pressed) {
-        layer_off(_PLOVER);
-      }
-      return false;
-      break;
-  }
-  return true;
+        break;
+    case M_FWRD:
+        /* Command + ] or next page */
+        if (record->event.pressed) {
+            if (use_cmd) {
+                SEND_STRING(SS_LGUI(SS_TAP(X_RBRC)));
+            } else {
+                SEND_STRING(SS_LALT(SS_TAP(X_RIGHT)));
+            }
+        }
+        break;
+    case M_PTAB:
+        /* Command + { or prev tab. */
+        if (record->event.pressed) {
+            if (use_cmd) {
+                SEND_STRING(SS_LGUI(SS_RSFT(SS_TAP(X_LBRC))));
+            } else {
+                SEND_STRING(SS_LCTRL(SS_RSFT(SS_TAP(X_TAB))));
+            }
+        }
+        break;
+    case M_NTAB:
+        /* Command + } or next tab*/
+        if (record->event.pressed) {
+            if (use_cmd) {
+                SEND_STRING(SS_LGUI(SS_RSFT(SS_TAP(X_RBRC))));
+            } else {
+                SEND_STRING(SS_LCTRL(SS_TAP(X_TAB)));
+            }
+        }
+        break;
+    case M_ZOUT:
+        /* Command + - or Ctrl + -*/
+        if (record->event.pressed) {
+            if (use_cmd) {
+                SEND_STRING(SS_LGUI(SS_TAP(X_MINS)));
+            } else {
+                SEND_STRING(SS_LCTRL(SS_TAP(X_MINS)));
+            }
+        }
+        break;
+    case M_ZOIN:
+        /* Command + = or Ctrl + =*/
+        if (record->event.pressed) {
+            if (use_cmd) {
+                SEND_STRING(SS_LGUI(SS_TAP(X_EQL)));
+            } else {
+                SEND_STRING(SS_LCTRL(SS_TAP(X_EQL)));
+            }
+        }
+        break;
+    case M_ENTR:
+        /* Command + Enter or Ctrl + Enter*/
+        if (record->event.pressed) {
+            if (use_cmd) {
+                SEND_STRING(SS_LGUI(SS_TAP(X_ENT)));
+            } else {
+                SEND_STRING(SS_LCTRL(SS_TAP(X_ENT)));
+            }
+        }
+        break;
+    case M_NSPC:
+        /* Next space / desktop */
+        if (record->event.pressed) {
+            if (use_cmd) {
+                SEND_STRING(SS_LCTRL(SS_TAP(X_RIGHT)));
+            } else {
+                SEND_STRING(SS_LGUI(SS_TAP(X_RIGHT)));
+            }
+        }
+        break;
+    case M_PSPC:
+        /* Previous space / desktop */
+        if (record->event.pressed) {
+            if (use_cmd) {
+                SEND_STRING(SS_LCTRL(SS_TAP(X_LEFT)));
+            } else {
+                SEND_STRING(SS_LGUI(SS_TAP(X_LEFT)));
+            }
+        }
+        break;
+    case M_APPN:
+        /* Next or previous app */
+        if (record->event.pressed) {
+            if (use_cmd) {
+                SEND_STRING(SS_LGUI(SS_TAP(X_GRV)));
+            } else {
+                SEND_STRING(SS_LALT(SS_TAP(X_TAB)));
+            }
+            break;
+        }
+}
+return true;
 }
