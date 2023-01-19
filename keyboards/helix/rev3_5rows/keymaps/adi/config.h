9c9d404412cf6b7331f4a4560c9155d75bfd40c7
Adi Ron <adi@adiron.me>
2023-01-19 17:55:19 +0200

    Fixed an issue with helixfont

---
+++ keyboards/helix/rev3_5rows/keymaps/adi/config.h
@@ -0,0 +1,5 @@
+// This sets a custom font. Somehow it wouldn't work without it.
+#ifdef OLED_FONT_H
+    #undef OLED_FONT_H
+#endif
+#define OLED_FONT_H "keyboards/helix/rev3_5rows/keymaps/adi/helixfont.h"
