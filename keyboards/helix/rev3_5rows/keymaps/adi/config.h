434e8530a18581868a2ce7f5c4d8784cba409e9f
Adi Ron <adi@adiron.me>
2023-02-08 08:28:29 +0200

    Changed the sides due to... hardware failure

--- keyboards/helix/rev3_5rows/keymaps/adi/config.h
+++ keyboards/helix/rev3_5rows/keymaps/adi/config.h
@@ -2,4 +2,13 @@
 #ifdef OLED_FONT_H
     #undef OLED_FONT_H
 #endif
+#ifdef MASTER_LEFT
+    #undef MASTER_LEFT
+#endif
+#ifdef SPLIT_HAND_MATRIX_GRID
+    #undef SPLIT_HAND_MATRIX_GRID
+    #undef SPLIT_HAND_MATRIX_GRID_LOW_IS_RIGHT
+#endif
+// #define MASTER_LEFT
+#define MASTER_RIGHT
 #define OLED_FONT_H "keyboards/helix/rev3_5rows/keymaps/adi/helixfont.h"
